12651c9a52db169ff164f3e9b8d7196c7fd4c5b2
pract <kimc570612@gmail.com>
2025-12-21 14:25:26 +0900

    'efeffff'

--- voice/views.py
+++ voice/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from book.models import VoiceList, MyVoiceList, VoiceType
+from django.db.models import Count
 
 # Create your views here.
 
@@ -18,13 +19,23 @@ def voice_list(request):
     
 
     # 타입 필터링
-    selected_type_ids  = request.GET.getlist('type_id')
+    selected_type_ids = request.GET.getlist('type_id')
 
-    if selected_type_ids :
-        voice_lists = VoiceList.objects.filter(types__id__in=selected_type_ids ).distinct()
-    else:
-        voice_lists = VoiceList.objects.all()
+    voice_lists = VoiceList.objects.all()
 
+    if selected_type_ids:
+        voice_lists = (
+            voice_lists
+            .filter(types__id__in=selected_type_ids)
+            .annotate(
+                match_count=Count(
+                    'types',
+                    filter=Q(types__id__in=selected_type_ids),
+                    distinct=True
+                )
+            )
+            .filter(match_count=len(selected_type_ids))
+        )
     # 모든 VoiceType 가져오기
     voice_types = VoiceType.objects.all()
 
